1d065847f55eb57777787bb9031edd09de04f77c
Eric Ferreira <eric.ferreira@valecard.com.br>
2022-09-28 14:38:44 -0300

    chore: deploy gh pages

--- lib/alphabet_search_view.dart
+++
@@ -1,6 +0,0 @@
-library alphabet_search_view;
-
-export 'src/constants.dart';
-export 'src/alphabet_search_view.dart';
-export 'src/alphabet_search_model.dart';
-export 'src/alphabet_search_decoration.dart';
